4db50f18628bcba098b199dd40c3b15e4b27705b
NamasteSmit <smitpatel1305@gmail.com>
2024-02-18 04:00:53 -0500

    UI

---
+++ domMain.js
@@ -0,0 +1,16 @@
+var element;
+
+// -------:Targeting Methods:-------
+
+// element = document.all;
+// element = document.links[4];
+// element = document.images;
+// element = document.forms;
+// element = document.body;
+//element = document.title;
+//element = document.head;
+// element = document.getElementById("#back");
+//  element = document.getElementsByClassName("navBar");
+// element = document.getElementsByClassName("anchor-panel")[1];
+element = document.getElementsByTagName("ul");
+console.log(element);
